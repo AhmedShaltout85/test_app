f31de18961131cdb07cf6bf13bf058092173d78e
AhmedShaltout85 <eng.abozaher@gmail.com>
2025-10-18 14:59:47 +0300

    last-update-18-10-2025

--- lib/bloc_training/repo/remote_repo_api.dart
+++ lib/bloc_training/repo/remote_repo_api.dart
@@ -1,14 +1,19 @@
-
 import 'package:dio/dio.dart';
 
 import '../model/post.dart';
 
 class RemoteRepoApi {
-  // static const String baseUrl = 'https://jsonplaceholder.typicode.com';
-
-  final dio = Dio();
+  static const String baseUrl = 'https://jsonplaceholder.typicode.com';
+  // Singleton
+  RemoteRepoApi._();
+  static final RemoteRepoApi _remoteRepoApi = RemoteRepoApi._();
+  var dio = Dio();
+  factory RemoteRepoApi() {
+    return _remoteRepoApi..dio = Dio();
+  }
+  // Get all posts
   Future<List<Post>> getPosts() async {
-    final response = await dio.get('https://jsonplaceholder.typicode.com/posts');
+    final response = await dio.get('$baseUrl/posts');
     if (response.statusCode == 200) {
       return Post.fromJsonList(response.data);
     } else {
@@ -16,4 +21,3 @@ class RemoteRepoApi {
     }
   }
 }
-
